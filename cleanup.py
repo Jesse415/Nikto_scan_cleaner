da0d833d5b6c9f19ba0a91225120b64ee2e416cc
one <one@unics.wireless.griffith.edu.au>
2020-09-24 11:30:46 +1000

    Working prototype

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,68 @@
+
 import sys
 import fnmatch
+
+def set_flag(strs):
+    subs = 'Target IP:'
+    host = 'Target Hostname:'
+    anti = 'anti-clickjacking'
+    xss = 'X-XSS-Protection'
+    cgi = 'No CGI'
+
+    #print(strs)
+    sbus = [i for i in strs if subs in i]
+    if sbus:
+        print(sbus)
+    else:
+        print("0")
+
+    tsoh = [i for i in strs if host in i]
+    if tsoh:
+        print(tsoh)
+    else:
+        print("0")
+
+    itna = [i for i in strs if anti in i]
+    if itna:
+        print(itna)
+    else:
+        print("0")
+
+    ssx = [i for i in strs if xss in i]
+    if ssx:
+        print(ssx)
+    else:
+        print("0")
+
+    igc = [i for i in strs if cgi in i]
+    if igc:
+        print(igc)
+    else:
+        print("0")
+
+
+
+
+
+
+               #     tmp = strs[line + 1] + ","
+               #     output.write(tmp)
+               # if strs[line] == 'Hostname:':
+               #   tmp = strs[line + 1] + ","
+               #   output.write(tmp)
+               #   #print(strs[line + 1], end=",")
+               # if strs[line] == 'anti-clickjacking':
+               #   tmp = "1,"
+               #   output.write(tmp)
+               #   #print("1", end=',')
+               # if strs[line] == 'X-XSS-Protection':
+               #   tmp = "1\n"
+               #   output.write(tmp)
+               # if strs[line] == 'All':
+               #     tmp = "1/n"
+               #     output.write(tmp)
+
+
 
 def filter_line(g):
 
@@ -8,25 +71,13 @@ def filter_line(g):
     output.write('IP' + ',' + 'Hostname' + ',' + 'Anti-clickjacking' + ',' + 'X-XSS-Protection' + '\n')
 
     for row in range(len(g)):
+        set_flag(g[row])
         #print(row, g[row], end="\n")
-        for col in range(len(g[row])):
-            strs=g[row][col].split()
-            for line in range(len(strs)):
-                if strs[line] == 'IP:':
-                    tmp = strs[line + 1] + ","
-                    output.write(tmp)
-                elif strs[line] == 'Hostname:':
-                    tmp = strs[line + 1] + ","
-                    output.write(tmp)
-                    #print(strs[line + 1], end=",")
-                elif strs[line] == 'anti-clickjacking':
-                    tmp = "1,"
-                    output.write(tmp)
-                    #print("1", end=',')
-                elif strs[line] == 'X-XSS-Protection':
-                    tmp = "1\n"
-                    output.write(tmp)
-
+#        for col in range(len(g[row])):
+#            set_flag(g[row][col])
+        #    strs=g[row][col].split()
+        #    for line in range(len(strs)):
+        #        print(strs[line])
 
 def create_graph(lists):
     pattern = '- Nikto*'
@@ -68,3 +119,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
