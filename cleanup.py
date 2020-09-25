afb5fcd4a70e065abf1ee84fa913cc40947e8681
one <one@unics.local>
2020-09-25 18:35:00 +1000

    full working version

--- cleanup.py
+++ cleanup.py
@@ -1,88 +1,63 @@
-
 import sys
 import fnmatch
 
-def set_flag(strs):
+def filter_line(g):
+    no = 'No web server found'
     subs = 'Target IP:'
     host = 'Target Hostname:'
+
+    # Flags to look for and mark
     anti = 'anti-clickjacking'
     xss = 'X-XSS-Protection'
     cgi = 'No CGI'
-
-    #print(strs)
-    sbus = [i for i in strs if subs in i]
-    if sbus:
-        print(sbus)
-    else:
-        print("0")
-
-    tsoh = [i for i in strs if host in i]
-    if tsoh:
-        print(tsoh)
-    else:
-        print("0")
-
-    itna = [i for i in strs if anti in i]
-    if itna:
-        print(itna)
-    else:
-        print("0")
-
-    ssx = [i for i in strs if xss in i]
-    if ssx:
-        print(ssx)
-    else:
-        print("0")
-
-    igc = [i for i in strs if cgi in i]
-    if igc:
-        print(igc)
-    else:
-        print("0")
-
-
-
-
-
-
-               #     tmp = strs[line + 1] + ","
-               #     output.write(tmp)
-               # if strs[line] == 'Hostname:':
-               #   tmp = strs[line + 1] + ","
-               #   output.write(tmp)
-               #   #print(strs[line + 1], end=",")
-               # if strs[line] == 'anti-clickjacking':
-               #   tmp = "1,"
-               #   output.write(tmp)
-               #   #print("1", end=',')
-               # if strs[line] == 'X-XSS-Protection':
-               #   tmp = "1\n"
-               #   output.write(tmp)
-               # if strs[line] == 'All':
-               #     tmp = "1/n"
-               #     output.write(tmp)
-
-
-
-def filter_line(g):
+    vrwj = 'valid response with junk' # May cause false positives
+    allow = 'Allowed HTTP Methods'
 
     path="CLEAN.csv"
     output=open(path, 'w')
-    output.write('IP' + ',' + 'Hostname' + ',' + 'Anti-clickjacking' + ',' + 'X-XSS-Protection' + '\n')
+    output.write('IP' + ',' + 'Hostname' + ',' + 'Anti-clickjacking' + ',' + 'X-XSS-Protection' + ',' +
+                 'No CGI Directories found' + ',' + 'valid response with junk HTTP' + ',' +
+                 'Allowed HTTP Methods' + '\n')
 
+    # Each "row" is equal to one Nikto scan
+    # If Nikto failed row will be < 3
     for row in range(len(g)):
-        set_flag(g[row])
-        #print(row, g[row], end="\n")
-#        for col in range(len(g[row])):
-#            set_flag(g[row][col])
-        #    strs=g[row][col].split()
-        #    for line in range(len(strs)):
-        #        print(strs[line])
+        if len(g[row])>3:
+            strs = g[row]
+            flags = ['0','0','0','0','0']   # Reset flags after every scan
+            for scan in range(len(strs)):
+                if subs in strs[scan]:
+                    tmp = strs[scan].split()[3] + ','
+                    output.write(tmp)
+                elif host in strs[scan]:
+                    tmp = strs[scan].split()[3]
+                    output.write(tmp)
+                elif no in strs[scan]:
+                    break
+            # Set flags to 1 for every mark
+            if [i for i in strs if anti in i]:
+                flags[0] = '1'
+            if [i for i in strs if xss in i]:
+                flags[1] = '1'
+            if [i for i in strs if cgi in i]:
+                flags[2] = '1'
+            if [i for i in strs if vrwj in i]:
+                flags[3] = '1'
+            if [i for i in strs if allow in i]:
+                flags[4] = '1'
+
+            # If the scan Failed or no web server found, skip write to file
+            if no not in strs[scan]:
+                for flag in range(len(flags)):
+                    output.write(',' + str(flags[flag]))
+                output.write('\n')
+
+    output.close()
 
 def create_graph(lists):
     pattern = '- Nikto*'
     graph = []
-    count = -2
+    count = -1
 
     # Make an array for Each Nikto Scan with each
     # array variable being a new line
@@ -104,13 +79,14 @@ def create_graph(lists):
 def main():
     items = []
     array_lines = []
+    header = ('IP,Hostname,Anti-clickjacking,X-XSS-Protection\n')
 
     try:
         #f=open("testInput.txt", "r")
         with open('testInput.txt') as f:
             # Read in Everyline
             for line in f:
-                items.append(line)
+                items.append(line.replace('\n',''))
 
     finally:
         f.close()
